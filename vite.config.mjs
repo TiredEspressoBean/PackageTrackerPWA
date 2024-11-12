3a0f85cc919bc0a9b701751dfb0c533d33c41b1b
TiredEspressoBean <isherwoc@student.osu.edu>
2024-11-11 19:00:06 -0500

    Initial commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,30 +6,33 @@ import {VitePWA} from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [react(), tsconfigPaths(),
 	  VitePWA({
-	  registerType: 'autoUpdate', // Automatically updates service worker
-	  includeAssets: ['favicon.svg', 'robot.txt'],
-	  manifest: {
-		  name: 'Where\'s My Package',
-		  short_name: 'WMP',
-		  description: 'A package tracking applications',
-		  theme_color: '#47cab4ff',
-		  background_color: '#ffffff',
-		  start_url: '/',
-		  display: 'standalone',
-		  icons: [
+		  "name": "Where's My Package",
+		  "short_name": "WMP",
+		  "start_url": "/",
+		  "display": "standalone",
+		  "background_color": "#ffffff",
+		  "lang": "en",
+		  "scope": "/",
+		  "description": "A package tracking application",
+		  "theme_color": "#47cab4",
+		  "icons": [
 			  {
-				  src: '/public/icons/manifest-icon-192.maskable.png',
-				  sizes: '192x192',
-				  type: 'image/png',
+				  "src": "/public/icons/manifest-icon-192.maskable.png",
+				  "sizes": "192x192",
+				  "type": "image/png"
 			  },
 			  {
-				  src: '/public/icons/manifest-icon-512.maskable.png',
-				  sizes: '512x512',
-				  type: 'image/png',
-			  },
+				  "src": "/public/icons/manifest-icon-512.maskable.png",
+				  "sizes": "512x512",
+				  "type": "image/png"
+			  }
 		  ],
-	  },
-  }),
+		  "categories": [
+			  "productivity",
+			  "shopping",
+			  "utilities"
+		  ]
+	  }),
   ],
   test: {
     globals: true,
