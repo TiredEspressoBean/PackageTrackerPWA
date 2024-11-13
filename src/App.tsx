15f1dcbef56949d83677e52fccb4992e418c22d7
TiredEspressoBean <isherwoc@student.osu.edu>
2024-11-12 23:41:32 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import axios from "axios";
+
+axios.defaults.withCredentials = true
 
 export default function App() {
   return (
